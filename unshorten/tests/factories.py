7fdcd9f9d36df6bf4b80548636f434efc0f5c1de
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-10 16:39:24 +0100

    Added factories for the models of the unshorten app.

---
+++ unshorten/tests/factories.py
@@ -0,0 +1,17 @@
+"""Factories for the models of the ``unshorten`` app."""
+from factory import Factory, SubFactory
+
+from django_libs.tests.factories import UserFactory
+
+from unshorten.models import APICallDayHistory
+
+
+class APICallDayHistoryFactory(Factory):
+    FACTORY_FOR = APICallDayHistory
+
+    amount_api_calls = 2500
+    user = SubFactory(UserFactory)
+
+
+class APICallMonthHistoryFactory(APICallDayHistoryFactory):
+    FACTORY_FOR = APICallDayHistory
