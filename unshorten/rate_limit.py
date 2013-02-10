a6e60950a27e9d691a3b3e4d06b757ecb7985b40
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-10 15:21:20 +0100

    codereview fixes for da223d8

--- unshorten/rate_limit.py
+++ unshorten/rate_limit.py
@@ -1,4 +1,7 @@
 """Base classes for rate limiting."""
+from django.utils.timezone import now
+
+from unshorten.models import APICallDayHistory
 
 
 class RateLimit(object):
@@ -6,7 +9,19 @@ class RateLimit(object):
 
     def __init__(self, request):
         self.user = request.user
+        self.request = request
 
     def is_rate_limit_exceeded(self):
         # TODO At the moment, this serves only testing purposes.
         return False
+
+    def log_api_call(self):
+        try:
+            history = APICallDayHistory.objects.get(
+                user=self.request.user, creation_date=now().date())
+        except APICallDayHistory.DoesNotExist:
+            history = APICallDayHistory(user=self.request.user)
+            history.amount_api_calls = 1
+        else:
+            history.amount_api_calls += 1
+        history.save()
--- unshorten/views.py
+++ unshorten/views.py
@@ -7,36 +7,26 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
-from django.utils.timezone import now
 from django.views.generic import View
 
 from unshorten.backend import RateLimit
-from unshorten.models import APICallDayHistory
 
 
 class UnshortenAPIView(View):
     """API view to handle the unshortening."""
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        rate_limit = RateLimit(request)
+        self.rate_limit = RateLimit(request)
 
         # checking if rate limit is exceeded
-        if rate_limit.is_rate_limit_exceeded(request.user):
+        if self.rate_limit.is_rate_limit_exceeded():
             raise PermissionDenied
         self.short_url = request.GET.get('url')
         return super(UnshortenAPIView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         if self.short_url:
-            try:
-                history = APICallDayHistory.objects.get(
-                    user=request.user, creation_date=now().date())
-            except APICallDayHistory.DoesNotExist:
-                history = APICallDayHistory(user=request.user)
-                history.amount_api_calls = 1
-            else:
-                history.amount_api_calls += 1
-            history.save()
+            self.rate_limit.log_api_call()
             try:
                 resp = urllib2.urlopen(self.short_url)
             except (
