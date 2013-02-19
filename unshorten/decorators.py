8ee08fb8e0690c26ef045c17ec753956e6992ecd
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-19 10:25:02 +0100

    codereview fixes for 6fad337
    cleaned up the decorator

--- unshorten/decorators.py
+++ unshorten/decorators.py
@@ -24,18 +24,17 @@ def get_username(identifier):
 def http_auth(func):
     @wraps(func)
     def _decorator(request, *args, **kwargs):
-        if not request.user.is_authenticated():
-            if "HTTP_AUTHORIZATION" in request.META.keys():
-                authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
-                if authmeth.lower() == 'basic':
-                    auth = auth.strip().decode('base64')
-                    identifier, password = auth.split(':', 1)
-                    username = get_username(identifier)
-                    user = authenticate(username=username, password=password)
-                    if user:
-                        login(request, user)
-                        return func(request, *args, **kwargs)
-        else:
+        if request.user.is_authenticated():
             return func(request, *args, **kwargs)
+        if 'HTTP_AUTHORIZATION' in request.META.keys():
+            authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+            if authmeth.lower() == 'basic':
+                auth = auth.strip().decode('base64')
+                identifier, password = auth.split(':', 1)
+                username = get_username(identifier)
+                user = authenticate(username=username, password=password)
+                if user:
+                    login(request, user)
+                    return func(request, *args, **kwargs)
         raise Http404
     return _decorator
