f6f93566e50f1060547738922358a3005b038126
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-20 11:41:39 +0100

    Added better model admins.

--- unshorten/admin.py
+++ unshorten/admin.py
@@ -8,6 +8,24 @@ from unshorten.models import (
 )
 
 
-admin.site.register(APICallDayHistory)
-admin.site.register(APICallMonthHistory)
-admin.site.register(UnshortenURL)
+class APICallDayHistoryAdmin(admin.ModelAdmin):
+    """Custom admin for the ``APICallDayHistory`` model."""
+    list_display = ['creation_date', 'email', 'amount_api_calls']
+    list_filter = ('user__email', )
+
+    def email(self, obj):
+        return obj.user.email
+
+
+class APICallMonthHistoryAdmin(APICallDayHistoryAdmin):
+    """Custom admin for the ``APICallMonthHistory`` model."""
+
+
+class UnshortenURLAdmin(admin.ModelAdmin):
+    """Custom admin for the ``UnshortenURL`` model."""
+    list_display = ['short_url', 'long_url', 'creation_date']
+
+
+admin.site.register(APICallDayHistory, APICallDayHistoryAdmin)
+admin.site.register(APICallMonthHistory, APICallMonthHistoryAdmin)
+admin.site.register(UnshortenURL, UnshortenURLAdmin)
