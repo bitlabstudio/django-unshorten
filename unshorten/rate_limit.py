41440778d7ccf4c993cdcd1538b0e2867f35aee3
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-20 11:42:02 +0100

    Saving history on rate limit class to save a db call.

--- unshorten/rate_limit.py
+++ unshorten/rate_limit.py
@@ -13,23 +13,25 @@ class SimpleRateLimit(object):
 
     def is_rate_limit_exceeded(self):
         try:
-            history = APICallDayHistory.objects.get(
+            # since we always check this first, we tie this to the class for
+            # later use
+            self.history = APICallDayHistory.objects.get(
                 user=self.user, creation_date=now().date())
         except APICallDayHistory.DoesNotExist:
             pass
         else:
-            if history.amount_api_calls >= settings.UNSHORTEN_DAILY_LIMIT:
+            if self.history.amount_api_calls >= settings.UNSHORTEN_DAILY_LIMIT:
                 return True
         return False
 
     def log_api_call(self):
-        try:
-            history = APICallDayHistory.objects.get(
-                user=self.user, creation_date=now().date())
-        except APICallDayHistory.DoesNotExist:
-            history = APICallDayHistory(user=self.user)
-            history.amount_api_calls = 1
-        else:
-            history.amount_api_calls += 1
-        history.save()
-        return history
+        if not hasattr(self, 'history'):
+            try:
+                self.history = APICallDayHistory.objects.get(
+                    user=self.user, creation_date=now().date())
+            except APICallDayHistory.DoesNotExist:
+                self.history = APICallDayHistory(user=self.user)
+                self.history.amount_api_calls = 1
+        self.history.amount_api_calls += 1
+        self.history.save()
+        return self.history
