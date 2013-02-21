2861c7baccbda698608d07b8480f805e7cfe866c
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-21 16:53:26 +0100

    Importing api auth class from settings.

--- unshorten/backend.py
+++ unshorten/backend.py
@@ -2,8 +2,16 @@
 from django.conf import settings
 
 
+# importing RateLimit class from settings
 class_name = settings.UNSHORTEN_RATE_LIMIT_CLASS.split('.')[-1]
 module_name = '.'.join(
     settings.UNSHORTEN_RATE_LIMIT_CLASS.split('.')[:-1])
 module = __import__(module_name, fromlist=[class_name])
 RateLimit = getattr(module, class_name)
+
+# importing APIAuthentication from settings
+class_name = settings.UNSHORTEN_API_AUTH_CLASS.split('.')[-1]
+module_name = '.'.join(
+    settings.UNSHORTEN_API_AUTH_CLASS.split('.')[:-1])
+module = __import__(module_name, fromlist=[class_name])
+APIAuthentication = getattr(module, class_name)
