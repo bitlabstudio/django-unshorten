e00124aac33f9c5496784ec73bf92021e1c7197e
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-02-21 09:53:37 +0100

    codereview fixes for 4144077

--- unshorten/rate_limit.py
+++ unshorten/rate_limit.py
@@ -11,7 +11,20 @@ class SimpleRateLimit(object):
     def __init__(self, request):
         self.user = request.user
 
+    def get_history(self):
+        """Returns the history from cache or DB or a newly created one."""
+        if hasattr(self, '_history'):
+            return self._history
+        try:
+            self._history = APICallDayHistory.objects.get(
+                user=self.user, creation_date=now().date())
+        except APICallDayHistory.DoesNotExist:
+            self._history = APICallDayHistory(user=self.user)
+            self._history.amount_api_calls = 0
+        return self._history
+
     def is_rate_limit_exceeded(self):
+        """Returns ``True`` if the rate limit is exceeded, otherwise False."""
         try:
             # since we always check this first, we tie this to the class for
             # later use
@@ -25,13 +38,8 @@ class SimpleRateLimit(object):
         return False
 
     def log_api_call(self):
-        if not hasattr(self, 'history'):
-            try:
-                self.history = APICallDayHistory.objects.get(
-                    user=self.user, creation_date=now().date())
-            except APICallDayHistory.DoesNotExist:
-                self.history = APICallDayHistory(user=self.user)
-                self.history.amount_api_calls = 1
-        self.history.amount_api_calls += 1
-        self.history.save()
-        return self.history
+        """Increases the amount of logged API calls for the user by 1."""
+        history = self.get_history()
+        history.amount_api_calls += 1
+        history.save()
+        return history
